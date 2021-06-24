3568c111f444c858d7a54fec15a41b867349e6f9
Gabriel Toschi <gabriel.toschi@hotmail.com>
2021-06-23 22:35:06 -0300

    Creates best first search algorithm

---
+++ best-first.py
@@ -0,0 +1,39 @@
+import graph as Graph
+from queue import PriorityQueue
+
+def bestFirst(graph: Graph.Graph, start, end):
+    visited = set()
+
+    parent = dict()
+    parent[start] = None
+
+    queue = PriorityQueue()
+    queue.put((0, start))
+    visited.add(start)
+
+    while queue.empty() == False:
+        current = queue.get()[1]
+
+        if current == end:
+            break
+
+        for v in graph.neighbors(current):
+            if v not in visited:
+                queue.put((Graph.distance(current, v), v))
+                visited.add(v)
+                parent[v] = current
+
+    foundPath = []
+    pathCurrent = end
+
+    if end in parent:
+        while pathCurrent != None:
+            foundPath.append(pathCurrent)
+            pathCurrent = parent[pathCurrent]
+
+    foundPath.reverse()
+
+    return foundPath
+
+test = Graph.KNNGraph(800, 5)
+print('Best First =========', bestFirst(test, test.vertices()[0], test.vertices()[1]))
